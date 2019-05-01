729c47124bcb56190fd2881bf7150e77dc769b5b
victor <victorokr@hotmail.com>
2019-05-01 00:24:24 -0500

    mi primer commit, login users por defecto, modificado a tabla empleado

---
+++ app/Empleado.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use App\Notifications\ResetPasswordNotificationEs;
+use Illuminate\Foundation\Auth\User as Authenticatable; //se coloco esta linea en remplazo de eloquent
+//use Illuminate\Database\Eloquent\Model;
+
+class Empleado extends Authenticatable
+{
+    use Notifiable;	
+
+    protected $table = 'empleado';
+    protected $primaryKey = 'id_empleado';
+     protected $fillable = ['Nombre','Apellidos','Telefono','email','password','Direccion','remember_token'];
+    protected $guard = 'employes';
+
+
+
+    // protected $hidden = [
+    //     'password', 'remember_token',
+    // ];
+}
